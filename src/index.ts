55228280aca7451a228dcf1dd673a1701366a9bd
Muhammed Mashood Alungal <muhdmashoodalungal@gmail.com>
2025-08-22 21:35:26 +0530

    feat : error handling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import { Elysia } from "elysia";
 import { checkDbConnection } from "./utils";
 import { bookApp } from "./routes";
+import { HttpError } from "./exceptions";
+import { errorResponse } from "./utils/response.util";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
 const app = new Elysia()
-  .onError(({ code, error }) => {
-    console.log("ERRRO R+++++++++++++=");
-
-    return new Response(error.toString());
+  .error("HTTP_ERROR", HttpError)
+  .onError(({ code, error, set }) => {
+    if (error instanceof HttpError) {
+      set.status = error.status;
+      errorResponse(error.status, error.message);
+    }
+    set.status = StatusCodes.INTERNAL_SERVER_ERROR;
+    errorResponse(StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR);
   })
   .get("/", () => "Hello Elysia")
   .use(bookApp)
